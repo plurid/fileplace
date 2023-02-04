f579f06e1997e1f21b9f837882ab530e547d7d69
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-02-04 17:41:39 +0200

    server/store > dev: store

--- packages/server/store/src/routes/store.rs
+++ packages/server/store/src/routes/store.rs
@@ -1,6 +1,10 @@
-use actix_web::HttpResponse;
+use std::fs;
+use actix_web::{HttpResponse, ResponseError};
+use actix_web::http::StatusCode;
 use actix_easy_multipart::tempfile::Tempfile;
 use actix_easy_multipart::MultipartForm;
+use mime_guess::get_mime_extensions;
+use uuid::Uuid;
 
 
 
@@ -12,8 +16,30 @@ pub struct Upload {
 
 
 pub async fn store(form: MultipartForm<Upload>) -> HttpResponse {
-    dbg!("{}", &form.files[0].file);
-    dbg!("{}", &form.files[0].file_name);
+    let temp_file = &form.files[0];
+    let named_temp_file = &temp_file.file;
+    let temp_path =  named_temp_file.path().as_os_str();
+    let content_type = match &temp_file.content_type {
+        Some(val) => match get_mime_extensions(val) {
+            Some(v) => {
+                let mut iter = v.into_iter();
+                match iter.next() {
+                    Some(value) => value.to_string(),
+                    None => String::new(),
+                }
+            },
+            None => String::new(),
+        },
+        None => String::new(),
+    };
+    let filename = Uuid::new_v4();
+    let path = format!("{}.{}", filename, content_type.clone());
 
-    HttpResponse::Ok().into()
+    match fs::rename(
+        temp_path,
+        path,
+    ) {
+        Ok(_) => HttpResponse::Ok().into(),
+        Err(_) => HttpResponse::BadRequest().into(),
+    }
 }
