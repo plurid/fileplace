f96a02c87304ffdf96171a0ef135f84ca3268f4f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-02-05 14:52:44 +0200

    server/store > dev: routes/exists

--- packages/server/store/src/routes/exists.rs
+++ packages/server/store/src/routes/exists.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, HttpResponse};
+use std::path::{Path, PathBuf};
 
 use crate::routes::utils::{
     QueryData,
@@ -17,6 +18,22 @@ pub async fn exists(
         name,
         owner,
     } = extract_query_params(query);
+
+    let path;
+    if owner.is_empty() {
+        path = Path::new(data_path.as_str())
+            .join(place)
+            .join(name);
+    } else {
+        path = Path::new(data_path.as_str())
+            .join(owner)
+            .join(place)
+            .join(name);
+    }
+
+    if !path.exists() {
+        return HttpResponse::NotFound().into();
+    }
 
     HttpResponse::Ok().into()
 }
