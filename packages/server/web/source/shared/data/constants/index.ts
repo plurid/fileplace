eee4f4190e098386e0a07e467d314b0befddf4aa
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-02-06 16:16:11 +0200

    server/web > dev: shared/kernel/services/logic/files

--- packages/server/web/source/shared/data/constants/index.ts
+++ packages/server/web/source/shared/data/constants/index.ts
@@ -15,4 +15,7 @@ export const APPLICATION_ROOT = 'plurid-app';
 export const routerProperties: PluridRouterPartialProperties = {
     fadeIn: 10,
 };
+
+
+export const FILEPLACE_STORE_ENDPOINT = 'http://127.0.0.1:56165';
 // #endregion module
---
+++ packages/server/web/source/shared/kernel/services/logic/files/index.ts
@@ -0,0 +1,29 @@
+// #region imports
+    // #region libraries
+    import fetch from 'cross-fetch';
+    // #endregion libraries
+
+
+    // #region external
+    import {
+        FILEPLACE_STORE_ENDPOINT,
+    } from '~shared/data/constants';
+    // #endregion external
+// #endregion imports
+
+
+
+// #region module
+export const getPlaces = async () => {
+    try {
+        const response = await fetch(
+            FILEPLACE_STORE_ENDPOINT + '/places',
+        );
+        const data = await response.json();
+
+        return data.places;
+    } catch (error) {
+        return [];
+    }
+}
+// #endregion module
